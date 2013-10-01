fdd8b7ba947e57794432860ea6ed036c7fefa306
penguinpower <crice@mymail.mines.edu>
2013-09-30 19:31:52 -0600

    v1.2.21 Now with more output

--- src/merging_exercise/firstClass.java
+++ src/merging_exercise/firstClass.java
@@ -6,6 +6,7 @@ public class firstClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Goodbye, cruel world.");
+		System.out.println("Hello again, cruel world. I'm back!");
 	}
 
 }
