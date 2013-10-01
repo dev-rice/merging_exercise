ac18ac08d93f8cf85da32f51c743c2009c5bd6af
penguinpower <crice@mymail.mines.edu>
2013-09-30 19:28:07 -0600

    Added stuff to scarybranch

--- src/merging_exercise/dontlook.java
+++ src/merging_exercise/dontlook.java
@@ -5,6 +5,7 @@ public class dontlook {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Goodbye World");
+		System.out.println("I added stuff to this branch thats not on master");
 	}
 
 }
